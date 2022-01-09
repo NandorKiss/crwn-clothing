4f0e7b4c3efa030efa2789f5f164d6c411eb0305
Nandor Kiss <nfrkt3@gmail.com>
2022-01-09 14:42:37 -0500

    updating app to add Shop page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path ='/' element={<HomePage />} />
-        <Route path ='/hats' element={<HatsPage />} />
+        <Route path ='/shop' element={<ShopPage />} />
       </Routes>
       
     </div>
