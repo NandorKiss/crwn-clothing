d5efbd5286bebf07852db9b4cf3bbbe822897a53
Nandor Kiss <nfrkt3@gmail.com>
2022-01-09 10:27:13 -0500

    updated to allow navigation with Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path ='/' element={<HomePage />} />
+        <Route path ='/hats' element={<HatsPage />} />
+      </Routes>
+      
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import './menu-item.styles.scss'
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className ={`${size} menu-item`}>
+
+    
+
+function MenuItem({title, imageUrl, size, linkUrl, match}) {
+    const navigate = useNavigate();
+    return(
+    <div className ={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}>
         <div 
             className='background-image' 
             style={{
@@ -14,6 +20,6 @@ const MenuItem = ({title, imageUrl, size}) => (
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
-)
+)}
 
 export default MenuItem;
